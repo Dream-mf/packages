5101be7b967a204d498d98f0e3faf6856c9bee21
Chris Fryer <cfryerdev@gmail.com>
2024-05-15 15:39:15 -0600

    Add federation log

--- packages/dmf-logging/src/types.ts
+++ packages/dmf-logging/src/types.ts
@@ -3,35 +3,37 @@ export const debugPrefix = `[DREAM.MF-LOGGER]`;
 
 /** Log Config interface for the log listener */
 export interface LogConfig {
-    debug: Boolean,
-    logGeneral: Function | undefined,
-    logAuthentication: Function | undefined,
-    logException: Function | undefined,
-    logPageView: Function | undefined,
-    logFetch: Function | undefined,
-    logEvent: Function | undefined,
+  debug: Boolean;
+  logGeneral: Function | undefined;
+  logAuthentication: Function | undefined;
+  logException: Function | undefined;
+  logPageView: Function | undefined;
+  logFetch: Function | undefined;
+  logEvent: Function | undefined;
+  logFederation: Function | undefined;
 }
 
-/** The log levels, 
+/** The log levels,
  * Note: WIP and Not Used Yet  */
 export const LogLevel = {
-    Information: 0,
-    Warning: 1,
-    Error: 2
-}
+  Information: 0,
+  Warning: 1,
+  Error: 2,
+};
 
 /** Log type flag which is decorated in the event */
 export const LogType = {
-    General: 'GENERAL',
-    Authentication: 'AUTHENTICATION',
-    Exception: 'EXCEPTION',
-    PageView: 'PAGE_VIEW',
-    Fetch: 'FETCH',
-    Event: 'EVENT'
-}
+  General: "GENERAL",
+  Authentication: "AUTHENTICATION",
+  Exception: "EXCEPTION",
+  PageView: "PAGE_VIEW",
+  Fetch: "FETCH",
+  Event: "EVENT",
+  Federation: "FEDERATION",
+};
 
 export default {
-    LogLevel,
-    LogType,
-    debugPrefix
+  LogLevel,
+  LogType,
+  debugPrefix,
 };
