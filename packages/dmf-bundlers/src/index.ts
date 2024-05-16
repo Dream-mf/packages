e43c4e9e91ffa1d043145e17ec8bc1b551f50182
Chris Fryer <cfryerdev@gmail.com>
2024-05-16 15:21:09 -0600

    update exports

--- packages/dmf-bundlers/src/index.ts
+++ packages/dmf-bundlers/src/index.ts
@@ -2,7 +2,7 @@ import { withBaseWebpack } from "./webpack";
 import { withBaseRSPack } from "./rspack";
 import { DefaultRemoteName, DefaultContainerName } from "./common/types";
 
-export default {
+export {
   withBaseWebpack,
   withBaseRSPack,
   DefaultRemoteName,
