b5743dec64943b1ccebec4cfa3120e0cb272f1b4
Chris Fryer <cfryerdev@gmail.com>
2024-05-15 18:24:46 -0600

    Fixed runtime plugin register

--- packages/dmf-utilities/src/core.ts
+++ packages/dmf-utilities/src/core.ts
@@ -1,18 +1,23 @@
-import { init, validateRuntime, registerPlugin, RuntimeNotFoundError } from "@dream.mf/core";
+import {
+  init,
+  validateRuntime,
+  registerRuntimePlugin,
+  RuntimeNotFoundError,
+} from "@dream.mf/core";
 import { version } from "../package.json";
 
-/**  Validates the runtime is installed, if so, validates if the plugin is setup. 
+/**  Validates the runtime is installed, if so, validates if the plugin is setup.
  *   If its missing, it will set up the logger with the dream-mf runtime */
 export const setupRuntime = () => {
-    if (!validateRuntime()) {
-        console.warn(RuntimeNotFoundError);
-        init();
-    }
-    registerPlugin('utilities', {
-        package: `@dream.mf/utilities:${version}`,
-    });
-}
+  if (!validateRuntime()) {
+    console.warn(RuntimeNotFoundError);
+    init();
+  }
+  registerRuntimePlugin("utilities", {
+    package: `@dream.mf/utilities:${version}`,
+  });
+};
 
 export default {
-    setupRuntime
-}
+  setupRuntime,
+};
