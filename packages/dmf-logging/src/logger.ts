aa865ab3579eff421c63895c572682eb95797fb3
rcanfield <Rcanfield86@gmail.com>
2024-05-21 10:39:06 -0400

    Refactor logger

--- packages/dmf-logging/src/logger.ts
+++ packages/dmf-logging/src/logger.ts
@@ -1,77 +1,78 @@
-import { LogType } from "./types";
+import {
+	LogCommon,
+	LogEvent,
+	LogException,
+	LogInfo,
+	LogPageView,
+	LogType,
+} from "./types";
 
 /** Massage the details object s its a better fit for log event messages */
-const _extendDetail = (detail) => {
-  detail.properties
-    ? (detail.properties.location = window.location.href)
-    : (detail.properties = { location: window.location.href });
-  if (detail.properties) {
-    Object.keys(detail.properties).forEach((key) => {
-      if (typeof detail.properties[key] == "number") {
-        detail.properties[key] = detail.properties[key].toString();
-      }
-    });
-  }
-  return detail;
+const _extendDetail = <T extends LogCommon>(detail: T): T => {
+	detail.properties
+		? (detail.properties.location = window.location.href)
+		: (detail.properties = { location: window.location.href });
+	if (detail.properties) {
+		Object.keys(detail.properties).forEach((key) => {
+			if (typeof detail.properties[key] == "number") {
+				detail.properties[key] = detail.properties[key].toString();
+			}
+		});
+	}
+	return detail;
 };
 
 /** Log client instance which has function calls for logging and setting specific log type events */
 export const DreamMFLogClient = {
-  /** Function call forwarder setting custom event type to LogType General */
-  logGeneral: (detail) => {
-    detail = _extendDetail(detail);
-    const customEvent = new CustomEvent(LogType.General, { detail: detail });
-    window.dispatchEvent(customEvent);
-  },
-  /** Function call forwarder setting custom event type to LogType Authentication */
-  logAuthentication: (detail) => {
-    detail = _extendDetail(detail);
-    const customEvent = new CustomEvent(LogType.Authentication, {
-      detail: detail,
-    });
-    window.dispatchEvent(customEvent);
-  },
-  /** Function call forwarder setting custom event type to LogType Exception */
-  logException: (detail, exception) => {
-    detail = _extendDetail(detail);
-    const customEvent = new CustomEvent(LogType.Exception, {
-      detail: { ...detail, error: exception },
-    });
-    window.dispatchEvent(customEvent);
-  },
-  /** Function call forwarder setting custom event type to LogType Event */
-  logEvent: (detail) => {
-    detail = _extendDetail(detail);
-    const customEvent = new CustomEvent(LogType.Event, { detail: detail });
-    window.dispatchEvent(customEvent);
-  },
-  /** Function call forwarder setting custom event type to LogType Fetch */
-  logFetch: (detail) => {
-    detail = _extendDetail(detail);
-    const customEvent = new CustomEvent(LogType.Fetch, { detail: detail });
-    window.dispatchEvent(customEvent);
-  },
-  /** Function call forwarder setting custom event type to LogType PageView */
-  logPageView: (detail) => {
-    detail = _extendDetail(detail);
-    const customEvent = new CustomEvent(LogType.PageView, { detail: detail });
-    window.dispatchEvent(customEvent);
-  },
-  /** Function call forwarder setting custom event type to LogType Federation */
-  logFederation: (url, scope, module) => {
-    const customEvent = new CustomEvent(LogType.Federation, {
-      detail: { url, scope, module },
-    });
-    window.dispatchEvent(customEvent);
-  },
-  /** Function call forwarder setting custom event type to a LogType which you pass through */
-  log: (detail, loggerType) => {
-    detail = _extendDetail(detail);
-    const customEvent = new CustomEvent(loggerType, { detail: detail });
-    window.dispatchEvent(customEvent);
-  },
+	/** Function call forwarder setting custom event type to LogType General */
+	logInfo: (detail: LogInfo) => {
+		_extendDetail(detail);
+		const customEvent = new CustomEvent(LogType.Info, {
+			detail,
+		});
+		window.dispatchEvent(customEvent);
+	},
+	/** Function call forwarder setting custom event type to LogType Exception */
+	logException: (exception: LogException) => {
+		_extendDetail(exception);
+		const customEvent = new CustomEvent(LogType.Exception, {
+			detail: exception,
+		});
+		window.dispatchEvent(customEvent);
+	},
+	/** Function call forwarder setting custom event type to LogType Event */
+	logEvent: (event: LogEvent) => {
+		_extendDetail(event);
+		const customEvent = new CustomEvent(LogType.Event, { detail: event });
+		window.dispatchEvent(customEvent);
+	},
+	/** Function call forwarder setting custom event type to LogType PageView */
+	logPageView: (detail: LogPageView) => {
+		_extendDetail(detail);
+		const customEvent = new CustomEvent(LogType.PageView, {
+			detail,
+		});
+		window.dispatchEvent(customEvent);
+	},
+};
+
+export const createScopedLogger = (properties: LogCommon) => {
+	return {
+		logInfo: (info: LogInfo) => {
+			DreamMFLogClient.logInfo({ ...properties, ...info });
+		},
+		logException: (error: LogException) => {
+			DreamMFLogClient.logException({ ...properties, ...error });
+		},
+		logEvent: (event: LogEvent) => {
+			DreamMFLogClient.logEvent({ ...properties, ...event });
+		},
+		logPageView: (pageView: LogPageView) => {
+			DreamMFLogClient.logPageView({ ...properties, ...pageView });
+		},
+	};
 };
 
 export default {
-  DreamMFLogClient,
+	DreamMFLogClient,
 };
--- packages/dmf-logging/src/types.ts
+++ packages/dmf-logging/src/types.ts
@@ -3,37 +3,46 @@ export const debugPrefix = `[DREAM.MF-LOGGER]`;
 
 /** Log Config interface for the log listener */
 export interface LogConfig {
-  debug: Boolean;
-  logGeneral: Function | undefined;
-  logAuthentication: Function | undefined;
-  logException: Function | undefined;
-  logPageView: Function | undefined;
-  logFetch: Function | undefined;
-  logEvent: Function | undefined;
-  logFederation: Function | undefined;
+	debug: Boolean;
+	logInfo: Function | undefined;
+	logException: Function | undefined;
+	logPageView: Function | undefined;
+	logEvent: Function | undefined;
 }
 
 /** The log levels,
  * Note: WIP and Not Used Yet  */
 export const LogLevel = {
-  Information: 0,
-  Warning: 1,
-  Error: 2,
+	Information: 0,
+	Warning: 1,
+	Error: 2,
 };
 
 /** Log type flag which is decorated in the event */
 export const LogType = {
-  General: "GENERAL",
-  Authentication: "AUTHENTICATION",
-  Exception: "EXCEPTION",
-  PageView: "PAGE_VIEW",
-  Fetch: "FETCH",
-  Event: "EVENT",
-  Federation: "FEDERATION",
+	Info: "INFO",
+	Exception: "EXCEPTION",
+	PageView: "PAGE_VIEW",
+	Event: "EVENT",
 };
 
-export default {
-  LogLevel,
-  LogType,
-  debugPrefix,
+export type LogInfo = {
+	message: string;
+	level?: typeof LogLevel;
+} & LogCommon;
+
+export type LogException = {
+	error: string | Error;
+} & LogCommon;
+
+export type LogEvent = {
+	eventName: string;
+} & LogCommon;
+
+export type LogPageView = {
+	url: string;
+} & LogCommon;
+
+export type LogCommon = {
+	properties?: Record<string, string>;
 };
