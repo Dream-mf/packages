d20eee2fbc5ca82e387d5a6412ffd1de72969c30
Chris Fryer <cfryerdev@gmail.com>
2024-08-27 15:26:28 -0600

    update logger

--- packages/dmf-logging/src/types.ts
+++ packages/dmf-logging/src/types.ts
@@ -3,46 +3,47 @@ export const debugPrefix = `[DREAM.MF-LOGGER]`;
 
 /** Log Config interface for the log listener */
 export interface LogConfig {
-	debug: Boolean;
-	logInfo: Function | undefined;
-	logException: Function | undefined;
-	logPageView: Function | undefined;
-	logEvent: Function | undefined;
+  debug: Boolean;
+  logInfo: Function | undefined;
+  logException: Function | undefined;
+  logPageView: Function | undefined;
+  logEvent: Function | undefined;
 }
 
 /** The log levels,
  * Note: WIP and Not Used Yet  */
 export const LogLevel = {
-	Information: 0,
-	Warning: 1,
-	Error: 2,
+  Information: 0,
+  Warning: 1,
+  Error: 2,
 };
 
 /** Log type flag which is decorated in the event */
 export const LogType = {
-	Info: "INFO",
-	Exception: "EXCEPTION",
-	PageView: "PAGE_VIEW",
-	Event: "EVENT",
+  Info: "INFO",
+  Exception: "EXCEPTION",
+  PageView: "PAGE_VIEW",
+  Event: "EVENT",
 };
 
 export type LogInfo = {
-	message: string;
-	level?: typeof LogLevel;
+  message: string;
+  level?: typeof LogLevel;
 } & LogCommon;
 
 export type LogException = {
-	error: string | Error;
+  type: string;
+  error: string | Error | Object;
 } & LogCommon;
 
 export type LogEvent = {
-	eventName: string;
+  eventName: string;
 } & LogCommon;
 
 export type LogPageView = {
-	url: string;
+  url: string;
 } & LogCommon;
 
 export type LogCommon = {
-	properties?: Record<string, string>;
+  properties?: Record<string, string>;
 };
