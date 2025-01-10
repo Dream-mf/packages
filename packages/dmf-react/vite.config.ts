cbee9f2fae7b9943aec61b3e8e72aa14cebefa75
Russell Canfield <rcanfield86@gmail.com>
2025-01-10 16:19:53 -0500

    Update packages

--- packages/dmf-react/vite.config.ts
+++ packages/dmf-react/vite.config.ts
@@ -4,45 +4,44 @@ import { nxViteTsPaths } from "@nx/vite/plugins/nx-tsconfig-paths.plugin";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 import dts from "vite-plugin-dts";
-import { nodePolyfills } from "vite-plugin-node-polyfills";
 
 export default defineConfig({
-	root: __dirname,
-	cacheDir: "../../node_modules/.vite/packages/dmf-react",
-	plugins: [
-		react(),
-		nxViteTsPaths(),
-		nxCopyAssetsPlugin(["*.md"]),
-		dts({
-			entryRoot: "src",
-			tsconfigPath: join(__dirname, "tsconfig.lib.json"),
-		}),
-	],
-	build: {
-		outDir: "../../dist/packages/dmf-react",
-		emptyOutDir: true,
-		reportCompressedSize: true,
-		commonjsOptions: {
-			transformMixedEsModules: true,
-		},
-		lib: {
-			entry: "src/index.ts",
-			name: "react",
-		},
-		rollupOptions: {
-			external: (id: string) => {
-				return !/^[./]/.test(id);
-			},
-			output: [
-				{
-					entryFileNames: "index.mjs",
-					format: "es",
-				},
-				{
-					entryFileNames: "index.cjs",
-					format: "cjs",
-				},
-			],
-		},
-	},
+  root: __dirname,
+  cacheDir: "../../node_modules/.vite/packages/dmf-react",
+  plugins: [
+    react(),
+    nxViteTsPaths(),
+    nxCopyAssetsPlugin(["*.md"]),
+    dts({
+      entryRoot: "src",
+      tsconfigPath: join(__dirname, "tsconfig.lib.json"),
+    }),
+  ],
+  build: {
+    outDir: "../../dist/packages/dmf-react",
+    emptyOutDir: true,
+    reportCompressedSize: true,
+    commonjsOptions: {
+      transformMixedEsModules: true,
+    },
+    lib: {
+      entry: "src/index.ts",
+      name: "react",
+    },
+    rollupOptions: {
+      external: (id: string) => {
+        return !/^[./]/.test(id);
+      },
+      output: [
+        {
+          entryFileNames: "index.mjs",
+          format: "es",
+        },
+        {
+          entryFileNames: "index.cjs",
+          format: "cjs",
+        },
+      ],
+    },
+  },
 });
