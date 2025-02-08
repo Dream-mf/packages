5ce0acb3b085fdb0cb406c5e56ec14f05f0d8e9f
Chris Fryer <cfryerdev@gmail.com>
2025-02-08 13:54:11 -0700

    fix node_env ref

--- packages/dmf-bundlers/src/rspack/index.ts
+++ packages/dmf-bundlers/src/rspack/index.ts
@@ -1,15 +1,15 @@
-const path = require("node:path");
+const path = require('node:path');
 const {
   ModuleFederationPlugin,
-} = require("@module-federation/enhanced/rspack");
-const { defineConfig } = require("@rspack/cli");
-import rspack from "@rspack/core";
-import ReactRefreshPlugin from "@rspack/plugin-react-refresh";
-import DotenvPlugin from "dotenv-webpack";
-import defaults, { _defaultSupportedBrowsers } from "../common/defaults";
-import loaders from "../common/loaders";
-import rules from "../common/rules";
-import types from "../common/types";
+} = require('@module-federation/enhanced/rspack');
+const { defineConfig } = require('@rspack/cli');
+import rspack from '@rspack/core';
+import ReactRefreshPlugin from '@rspack/plugin-react-refresh';
+import DotenvPlugin from 'dotenv-webpack';
+import defaults, { _defaultSupportedBrowsers } from '../common/defaults';
+import loaders from '../common/loaders';
+import rules from '../common/rules';
+import types from '../common/types';
 
 /**
  * @param {Object} customConfig Config overrides to pass to the WebPackConfig and ModuleFederationPlugin.
@@ -19,23 +19,26 @@ import types from "../common/types";
 // biome-ignore lint/suspicious/noExplicitAny: <explanation>
 export const withBaseRspack = (customConfig: any, isTypescript: boolean) => {
   loaders._envLoader();
-  const prod = process.env?.NODE_ENV === "production";
+  let nodeEnv = 'development';
+  if (process && process.env && process.env.NODE_ENV) {
+    nodeEnv = process.env.NODE_ENV;
+  }
   return defineConfig({
-    entry: `./src/index.${isTypescript ? "ts" : "js"}`,
-    mode: prod ? "production" : "development",
+    entry: `./src/index.${isTypescript ? 'ts' : 'js'}`,
+    mode: nodeEnv ? 'production' : 'development',
     devtool:
-      (process.env?.NODE_ENV || "development") === "development"
-        ? "inline-source-map"
-        : "source-map",
+      (nodeEnv || 'development') === 'development'
+        ? 'inline-source-map'
+        : 'source-map',
     devServer: {
       ...defaults._defaultDevServer(customConfig.federationConfig.name),
       ...customConfig.devServer,
     },
     output: defaults._defaultOutput(customConfig.federationConfig.name),
     resolve: {
-      extensions: [".js", ".jsx", ".ts", ".tsx"],
+      extensions: ['.js', '.jsx', '.ts', '.tsx'],
       alias: {
-        "@shared": path.resolve(__dirname, "../shared"),
+        '@shared': path.resolve(__dirname, '../shared'),
       },
     },
     module: {
@@ -43,18 +46,18 @@ export const withBaseRspack = (customConfig: any, isTypescript: boolean) => {
         {
           test: /\.(j|t)s$/,
           exclude: [/[\\/]node_modules[\\/]/],
-          loader: "builtin:swc-loader",
+          loader: 'builtin:swc-loader',
           options: {
             jsc: {
               parser: {
-                syntax: "typescript",
+                syntax: 'typescript',
               },
               externalHelpers: false,
               transform: {
                 react: {
-                  runtime: "automatic",
-                  development: !prod,
-                  refresh: !prod,
+                  runtime: 'automatic',
+                  development: !nodeEnv,
+                  refresh: !nodeEnv,
                 },
               },
             },
@@ -66,19 +69,19 @@ export const withBaseRspack = (customConfig: any, isTypescript: boolean) => {
         {
           test: /\.(j|t)sx$/,
           exclude: [/[\\/]node_modules[\\/]/],
-          loader: "builtin:swc-loader",
+          loader: 'builtin:swc-loader',
           options: {
             jsc: {
               parser: {
-                syntax: "typescript",
+                syntax: 'typescript',
                 tsx: true,
               },
               externalHelpers: false,
               transform: {
                 react: {
-                  runtime: "automatic",
-                  development: !prod,
-                  refresh: !prod,
+                  runtime: 'automatic',
+                  development: !nodeEnv,
+                  refresh: !nodeEnv,
                 },
               },
             },
@@ -91,11 +94,11 @@ export const withBaseRspack = (customConfig: any, isTypescript: boolean) => {
           test: /\.(sass|scss)$/,
           use: [
             {
-              loader: "sass-loader",
+              loader: 'sass-loader',
               options: {},
             },
           ],
-          type: "css/auto",
+          type: 'css/auto',
         },
         ...rules._rulesAssets(),
       ],
@@ -105,16 +108,16 @@ export const withBaseRspack = (customConfig: any, isTypescript: boolean) => {
         systemvars: true,
       }),
       new rspack.HtmlRspackPlugin({
-        template: "./public/index.html",
-        favicon: "./public/favicon.ico",
-        inject: "body",
+        template: './public/index.html',
+        favicon: './public/favicon.ico',
+        inject: 'body',
       }),
       new ModuleFederationPlugin({
         shared: { ...defaults._defaultSharedModules() },
         filename: types.DefaultRemoteName,
         ...customConfig.federationConfig,
       }),
-      !prod && new ReactRefreshPlugin(),
+      !nodeEnv && new ReactRefreshPlugin(),
     ].filter(Boolean),
   });
 };
